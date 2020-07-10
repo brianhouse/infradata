6b02d0b4d820a99e511145ef4c5e9c61cac34d61
Brian House <email@brian.house>
2020-07-09 18:28:42 -0700

    gamma

--- rain.py
+++ rain.py
@@ -62,7 +62,7 @@ spectrum = normalize(spectrum)          # normalize
 print(f"--> time columns {len(ts)}")
 print(f"--> freq rows {len(freqs)}")
 
-spectrum **= 2.5    # compress high values a bit (gamma function)
+spectrum **= 2.8    # compress high values a bit (gamma function)
 
 ## draw
 print()
